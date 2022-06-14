cf2acbae4a1e241ca7bc99d84b2f5117e086442e
DarccMan <darcy3057@gmail.com>
2022-06-14 12:53:31 +1000

    Cors fix test

--- index.mjs
+++ index.mjs
@@ -1,8 +1,16 @@
 import express from "express";
-import path from "path";
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept",
+  );
+  next();
+});
 
 app.get("/test", (req, res) => {
   res.status(200).send("Hello from server! :)");
