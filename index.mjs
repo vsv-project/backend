4e27122c4fe50a8da68407c7413f440d5cd90be5
nslebruh <nslebrun1@gmail.com>
2022-06-17 22:20:26 +1000

    added environment variable

--- index.mjs
+++ index.mjs
@@ -7,6 +7,9 @@ import { initializeApp } from "firebase/app";
 // Start express
 const PORT = process.env.PORT || 3001;
 const app = express();
+const CONFIG = JSON.parse(process.env.CONFIG) 
+
+const firebase = initializeApp(CONFIG)
 
 // Allow CORS
 //! Change to strict domain
